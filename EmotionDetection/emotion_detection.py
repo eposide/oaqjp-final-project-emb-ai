760a07cedd0cc16d28de47e82010af909bb54693
FrikBriers <frik.briers@novadoc.nl>
2024-09-09 11:45:56 +0200

    7a_error_handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -13,17 +13,20 @@ def emotion_detector(text_to_analyze):
     data =  { "raw_document": { "text": text_to_analyze } }
     # do the call to the api    
     response = requests.post(url, json = data, headers=header)
+    result = dict.fromkeys("anger","discust", "fear", "joy", "sadness", "dominant_emotion");
+    if response.status_code == 400:
+    # return the initial result with None values 
+        return result
+    elif response.status_code == 200:
     # setup the result 
-    formatted_response = json.loads(response.text)
-    emotions = formatted_response['emotionPredictions'][0]['emotion']
-    result = {}
-    high = 0.0
-    dominant_emotion = ''   
-    for emotion, score in emotions.items():
-        result[emotion] = score
-        if float(score) > high: 
-            high = score
-            dominant_emotion = emotion 
-    result['dominant_emotion'] = dominant_emotion
-    return result;   
- 
+        formatted_response = json.loads(response.text)
+        emotions = formatted_response['emotionPredictions'][0]['emotion']
+        high = 0.0
+        dominant_emotion = ''
+        for emotion, score in emotions.items():
+            result[emotion] = score
+            if float(score) > high: 
+                high = score
+                dominant_emotion = emotion
+        result['dominant_emotion'] = dominant_emotion
+        return result;
--- server.py
+++ server.py
@@ -12,8 +12,9 @@ def emotion_detectection():
     '''
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
+    if response['dominant_emotion'] is None:
+        return "Invalid input! Try again."
     return f"For the given statement the system response is {response}"
-
 
 @app.route("/")
 def render_index_page():
